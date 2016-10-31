d6677e43fdc813eebb5bcb0a134dccfe61a642ff
Quinn Bast <Quinn.Bast@hotmail.com>
2016-10-31 12:24:22 -0600

    *Added a grid class with some stub functions

---
+++ Software/src/WildlifeSimulation/Grid.java
@@ -0,0 +1,21 @@
+package WildlifeSimulation;
+
+public class Grid 
+{
+	private Wildlife WildlifeGrid[][] = new Wildlife[50][50];
+	
+	public Grid() 
+	{
+		
+	}
+	
+	public void populateGrid()
+	{
+		
+	}
+
+	public void displayWildlife()
+	{
+		
+	}
+}
