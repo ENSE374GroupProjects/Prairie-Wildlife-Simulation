6ffe398bc4bfbd106c7cb930deab4933f2946766
Jonathon Wells <wellsjonathonr@gmail.com>
2016-11-10 18:08:26 -0600

    Implemented randomly-filled WildlifeGrid in populateGrid(), and displayGrid is properly displaying the grid

--- Software/src/WildlifeSimulation/Grid.java
+++ Software/src/WildlifeSimulation/Grid.java
@@ -12,20 +12,38 @@ public class Grid
 	{
 	}
 	
-	public void populateGrid(int wildlifeDensity)
+	private void populateGrid(int wildlifeDensity)
 	{
+		// Implement different chances for each specific creature to spawn
+		// Base the switch case value off of wildlifeDensity 
+		// eg: wildlifeDensity = 50, switch case for wolf (10%) = 0 - wildlifeDensity/10
 		int random;
 		
 		for (int i = 0; i < ROWS; i++)
 		{
 			for (int j = 0; j < COLS; j++)
 			{
-				random = ThreadLocalRandom.current().nextInt(1, 101);
+				random = ThreadLocalRandom.current().nextInt(0, 101);
 				if (random <= wildlifeDensity) {
-					WildlifeGrid[i][j] = new Wolf();
+					random = ThreadLocalRandom.current().nextInt(0, 12);
+					switch (random)
+					{
+					case 0:		WildlifeGrid[i][j] = new Tree(); break;
+					case 1: 	WildlifeGrid[i][j] = new Grass(); break;
+					case 2: 	WildlifeGrid[i][j] = new Caterpillar(); break;
+					case 3: 	WildlifeGrid[i][j] = new Grasshopper(); break;
+					case 4: 	WildlifeGrid[i][j] = new Rabbit(); break;
+					case 5: 	WildlifeGrid[i][j] = new Mouse(); break;
+					case 6: 	WildlifeGrid[i][j] = new Squirrel(); break;
+					case 7: 	WildlifeGrid[i][j] = new Bluejay(); break;
+					case 8: 	WildlifeGrid[i][j] = new Deer(); break;
+					case 9: 	WildlifeGrid[i][j] = new Fox(); break;
+					case 10: 	WildlifeGrid[i][j] = new Hawk(); break;
+					case 11: 	WildlifeGrid[i][j] = new Wolf(); break;
+					}
 				}
 				else {
-					System.out.print(" ");
+					WildlifeGrid[i][j] = null;
 				}
 			}
 		}
@@ -33,7 +51,7 @@ public class Grid
 
 	public void displayWildlife(int wildlifeDensity)
 	{
-		int random;
+		populateGrid(wildlifeDensity);
 		// Print top bar
 		System.out.print("/");
 		for (int i = 0; i < COLS; i++)
@@ -48,11 +66,12 @@ public class Grid
 			System.out.print("|");
 			for (int j = 0; j < COLS; j++)
 			{
-				random = ThreadLocalRandom.current().nextInt(1, 101);
-				if (random <= wildlifeDensity) {
-					System.out.print(".");
+				if (WildlifeGrid[i][j] != null)
+				{
+					System.out.print(WildlifeGrid[i][j].getSymbol());
 				}
-				else {
+				else
+				{
 					System.out.print(" ");
 				}
 			}
